6a75c60862671ac50bc8805099f9b70f764473b4
Abhishek Oza <abhishek@itoneclick.com>
2023-06-26 20:06:36 +0530

    fit: just new ui.(26th jun 23 at 20:06)

--- app/src/main/java/com/example/streamingapp/ui/components/adapter/TestimonyListAdapter.kt
+++ app/src/main/java/com/example/streamingapp/ui/components/adapter/TestimonyListAdapter.kt
@@ -1,17 +1,15 @@
 package com.example.streamingapp.ui.components.adapter
 
 import com.example.streamingapp.R
-import com.example.streamingapp.data.model.adapteritemlist.AwardGallery
 import com.example.streamingapp.data.model.adapteritemlist.TestimonyListItem
-import com.example.streamingapp.databinding.RawAwardGalleryItemLayoutBinding
-import com.example.streamingapp.databinding.RawTestimonyItemBinding
+import com.example.streamingapp.databinding.RawTestimonyItemNewBinding
 import com.example.streamingapp.utils.CustomAdapter
 import com.example.streamingapp.utils.setSafeOnClickListener
 
 object TestimonyListAdapter {
     fun getTestimonyAdapter(onShareListener: (item: TestimonyListItem) -> Unit = {}) = lazy {
-        CustomAdapter<TestimonyListItem, RawTestimonyItemBinding>(
-            layoutResource = R.layout.raw_testimony_item,
+        CustomAdapter<TestimonyListItem, RawTestimonyItemNewBinding>(
+            layoutResource = R.layout.raw_testimony_item_new,
             onBind = { binding, item, _ ->
                 binding.apply {
                     testimonyData = item
